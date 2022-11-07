0b69af4b1271cfb3305835f84027184438f44e75
Nicklas Hovgaard <nhl0110@gmail.com>
2022-11-07 16:32:03 +0100

    Commit 4, Package: controller, classes: guiController & gameController

---
+++ src/main/java/Controller/GUIcontroller.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Player;
+import gui_main.GUI;
+
+public class GUIcontroller {
+
+    GUI_Field[] gui_fieldArray;
+    GUI gui;
+    GUI_Player[] gui_players;
+
+}
---
+++ src/main/java/Controller/gameController.java
@@ -0,0 +1,14 @@
+package Controller;
+import Model.Board;
+
+public class gameController {
+    GUIcontroller guiController;
+
+    private Board gameBoard;
+
+    public gameController() {
+        GUIcontroller guiController;
+    }
+}
+
+// Basically main klassen, her skal vi lave, metoder, lave instanser
