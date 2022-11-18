9eeca823693c6b65cd80a1b5dae69792c5fcf98f
emil harchi <s223644@dtu.dk>
2022-11-18 11:34:11 +0100

    navn og pris

--- src/main/java/Model/FieldFactory.java
+++ src/main/java/Model/FieldFactory.java
@@ -11,30 +11,30 @@ public class FieldFactory {
         return new GUI_Field[]
 
                 {
-                        new GUI_Start("Start", "Modtag M2, når du passerer", "Modtag M2, når du passerer", Color.white, Color.black),
-                        new GUI_Street("Burgerbaren", "M1", " ", "M1", Color.orange, Color.black),
-                        new GUI_Street("Pizzahuset", "M1", " ", "M1", Color.orange, Color.black),
+                        new GUI_Start("Start", "Modtag $2, når du passerer", "Modtag M2, når du passerer", Color.white, Color.black),
+                        new GUI_Street("Burgerbaren", "$1", " Burgerbaren ", "$1", Color.orange, Color.black),
+                        new GUI_Street("Pizzahuset", "$1", "Pizzaria ", "$1", Color.orange, Color.black),
                         new GUI_Chance(),
-                        new GUI_Street("Slikbutikken", "M1", " ", "M1", Color.blue, Color.white),
-                        new GUI_Street("Iskiosken", "M1", " ", "M1", Color.blue, Color.white),
+                        new GUI_Street("Slikbutikken", "$1", "Slikbutikken ", "$1", Color.blue, Color.white),
+                        new GUI_Street("Iskiosken", "$1", "Iskiosken ", "$1", Color.blue, Color.white),
                         new GUI_Jail(),
-                        new GUI_Street("Museet", "M1", " ", "M1", Color.pink, Color.black),
-                        new GUI_Street("Biblioteket", "M1", " ", "M1", Color.pink, Color.black),
+                        new GUI_Street("Museet", "$2", " Museet", "$1", Color.pink, Color.black),
+                        new GUI_Street("Biblioteket", "$2", "Bibloteket ", "$1", Color.pink, Color.black),
                         new GUI_Chance(),
-                        new GUI_Street("Skaterparken", "M1", " ", "M1", Color.yellow, Color.black),
-                        new GUI_Street("Swimmingpoolen", "M1", " ", "M1", Color.yellow, Color.black),
+                        new GUI_Street("Skaterparken", "$2", " Skaterparken", "$2", Color.yellow, Color.black),
+                        new GUI_Street("Swimmingpoolen", "$2", "Swimmingpoolen ", "$2", Color.yellow, Color.black),
                         new GUI_Refuge(),
-                        new GUI_Street("Spillehallen", "M1", " ", "M1", Color.red, Color.black),
-                        new GUI_Street("Biografen", "M1", " ", "M1", Color.red, Color.black),
+                        new GUI_Street("Spillehallen", "$3", "Spillehallen ", "$3", Color.red, Color.black),
+                        new GUI_Street("Biografen", "$3", "Biografen ", "$3", Color.red, Color.black),
                         new GUI_Chance(),
-                        new GUI_Street("Legetøjsbutikken", "M1", " ", "M1", Color.cyan, Color.black),
-                        new GUI_Street("Dyrehandlen", "M1", " ", "M1", Color.cyan, Color.black),
+                        new GUI_Street("Legetøjsbutikken", "$3", "Legetøjsbutikken ", "$3", Color.cyan, Color.black),
+                        new GUI_Street("Dyrehandlen", "$3", "Dyrehandlen ", "$3", Color.cyan, Color.black),
                         new GUI_Jail(),
-                        new GUI_Street("Bowlinghallen", "M1", " ", "M1", Color.green, Color.black),
-                        new GUI_Street("Zoologisk Have", "M1", " ", "M1", Color.green, Color.black),
+                        new GUI_Street("Bowlinghallen", "$4", "Bowlinghallen", "$4", Color.green, Color.black),
+                        new GUI_Street("Zoologisk Have", "$4", "Zoologisk Have ", "$4", Color.green, Color.black),
                         new GUI_Chance(),
-                        new GUI_Street("Vandlandet", "M1", " ", "M1", Color.magenta, Color.black),
-                        new GUI_Street("Strandpromenaden", "M1", " ", "M1", Color.magenta, Color.black)
+                        new GUI_Street("Vandlandet", "$4", "Vandlandet ", "$4", Color.magenta, Color.black),
+                        new GUI_Street("Strandpromenaden", "$4", "Strandpromenaden ", "$4", Color.magenta, Color.black)
 
 
                 };
