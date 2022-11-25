f7c598f544b65204926f1b3174af4caccae9b290
Nicklas Hovgaard <nhl0110@gmail.com>
2022-11-25 12:04:39 +0100

    Commit DiceCupTest

---
+++ src/main/java/Model/DiceCupTest.java
@@ -0,0 +1,20 @@
+package Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceCupTest {
+
+    @Test
+    public void raffle() {
+        int numberOfRolls = 1000;
+        DiceCup diceCupTest = new DiceCup();
+        double sum = 0;
+        for (int i = 0; i < numberOfRolls; i++) {
+            sum += diceCupTest.raffle();
+        }
+        double gennemsnit = sum / numberOfRolls;
+        assertEquals(3.5, gennemsnit, 0.5);
+    }
+}
